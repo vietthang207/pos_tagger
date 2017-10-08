17a828b8d52f51c4f4ecc2dbd42829c3e159391b
Thang Dinh <vietthang207@gmail.com>
2017-10-09 01:53:32 +0800

    implement naive emission, transition prob (without smoothing) and Viterbi algo

--- src/HiddenMarkovModel.java
+++ src/HiddenMarkovModel.java
@@ -5,11 +5,13 @@ import java.util.ArrayList;
 public class HiddenMarkovModel {
 	private HashMap<String, Integer> nodeIndex;
 	private String[] tags;
+	private int numTags;
 	private HashSet<String> vocabulary;
 	private int[][] transitionCountMatrix;
 	private double[][] transitionProbabilityMatrix;
 	private ArrayList<HashMap<String, Integer>> emissionCount;
 	private ArrayList<HashMap<String, Double>> emissionProbabilities;
+	private boolean debug = true;
 
 	public HiddenMarkovModel() {
 		nodeIndex = new HashMap<String, Integer>();
@@ -20,6 +22,7 @@ public class HiddenMarkovModel {
 			nodeIndex.put(t, count);
 			count++;
 		}
+		numTags = count;
 		transitionCountMatrix = new int[count][count];
 		transitionProbabilityMatrix = new double[count][count];
 		emissionCount = new ArrayList<HashMap<String, Integer>>();
@@ -34,17 +37,18 @@ public class HiddenMarkovModel {
 		return nodeIndex.get(tag);
 	}
 
-	private int transitionCount(int state1, int state2) {
+	private int getTransitionCount(int state1, int state2) {
 		return transitionCountMatrix[state1][state1];
 	}
 
-	private double transitionProbability(int state1, int state2) {
+	private double getTransitionProbability(int state1, int state2) {
 		return transitionProbabilityMatrix[state1][state2];
 	}
 
-	private double emissionProbability(int state, String word) {
+	private double getEmissionProbability(int state, String word) {
 		if (!emissionProbabilities.get(state).containsKey(word)) {
 			// TODO: unknown word
+			return 0;
 		}
 		return emissionProbabilities.get(state).get(word);
 	}
@@ -71,7 +75,7 @@ public class HiddenMarkovModel {
 	}
 
 	public void processTrainingSample(String line) {
-		TaggedWord[] taggedWords = segmentLineIntoTaggedWord(line);
+		TaggedWord[] taggedWords = Util.segmentLineIntoTaggedWord(line);
 		addTransitionCount(Constant.TAG_START, taggedWords[0].getTag());
 		for (int i=0; i<taggedWords.length; i++) {
 			if (i == taggedWords.length-1) {
@@ -81,8 +85,81 @@ public class HiddenMarkovModel {
 				addTransitionCount(taggedWords[i].getTag(), taggedWords[i+1].getTag());
 			}
 			addVocabulary(taggedWords[i].getWord());
-			addEmissionCount(taggedWords[i].getWord);
+			addEmissionCount(taggedWords[i].getTag(), taggedWords[i].getWord());
 		}
+	}
+
+	public void calculateEmissionProbNaive() {
+		emissionProbabilities = new ArrayList<HashMap<String, Double>>();
+		if (debug) {
+			System.out.print("tags.length = " + tags.length);
+		}
+		for (int i=0; i<tags.length; i++) {
+			int sum = 0;
+			for (String observation: emissionCount.get(i).keySet()) {
+				sum += emissionCount.get(i).get(observation);
+			}
+			for (String observation: emissionCount.get(i).keySet()) {
+				int occurence = emissionCount.get(i).get(observation);
+				emissionProbabilities.get(i).put(observation, occurence*1.0/sum);
+			}
+		}
+	}
+
+	public void calculateTransitionProbNaive() {
+		if (debug) {
+			System.out.print("tags.length = " + tags.length);
+		}
+		for (int i=0; i<tags.length; i++) {
+			int sum = 0;
+			for (int j=0; j<tags.length; j++) {
+				sum += transitionCountMatrix[i][j];
+			}
+			for (int j=0; j<tags.length; j++) {
+				transitionProbabilityMatrix[i][j] = transitionCountMatrix[i][j]*1.0 / sum;
+			}
+		}
+	}
+
+	public String[] runViterbi(String[] words) {
+		int n = words.length + 2;
+		double[][] dp = new double[n][numTags];
+		int[][] trace = new int[n][numTags];
+		dp[0][0] = 1;
+		for (int i=1; i<n-1; i++) {
+			for (int j=0; j<numTags; j++) {
+				dp[i][j] = -1;
+				trace[i][j] = -1;
+				double b = getEmissionProbability(j, words[i-1]);
+				for (int k=0; k<numTags; k++) {
+					double tmp = dp[i-1][k] * getTransitionProbability(k, j) * b;
+					if (tmp > dp[i][j]) {
+						dp[i][j] = tmp;
+						trace[i][j] = k;
+					}
+				}
+			}
+		}
+		dp[n-1][numTags-1] = -1;
+		trace[n-1][numTags-1] = -1;
+		for (int k = 0; k<numTags; k++) {
+			double tmp = dp[n-1][k] * getTransitionProbability(k, numTags-1);
+			if (tmp > dp[n-1][numTags-1]) {
+				dp[n-1][numTags-1] = tmp;
+				trace[n-1][numTags-1] = k;
+			}
+		}
+		int[] res = new int[words.length];
+		int cur = numTags - 1;
+		for (int i=0; i<words.length; i++) {
+			res[i] = trace[n-1-i][cur];
+			cur = res[i];
+		}
+		String[] ret = new String[words.length];
+		for (int i=0; i<words.length; i++) {
+			ret[i] = tags[res[words.length-1-i]];
+		}
+		return ret;
 	}
 }
 
